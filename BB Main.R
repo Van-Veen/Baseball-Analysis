926ff70491424aa437162b54e27d00d2a8b4dd5f
J Stewart <30293862+Van-Veen@users.noreply.github.com>
2017-07-20 12:16:18 -0400

    Update BB Main.R

--- BB Main.R
+++ BB Main.R
@@ -33,25 +33,16 @@ batting <- function(playerName, atWork = F){
   x <- getPlayer(playerName, atWork = atWork)$playerID
   
   DF <- tableLoader("Batting", atWork = atWork) %>% 
-    .[playerID == x]
+    .[playerID == x] %>% 
+    setnames(., c("2B", "3B"), c("B2", "B3")) %>% 
+    .[, BA := round(H/AB, 3)] %>% 
+    .[, PA := AB + BB + HBP + SF + SH, by = c("yearID", "teamID")] %>% 
+    .[, OBP := round((H + BB + HBP)/(AB + BB + HBP + SF), 3)] %>% 
+    .[, SLG := round((((H - B2 - B3 - HR) + (B2*2) + (B3 * 3) + (HR * 4))/AB), 3)] %>% 
+    .[, OPS := OBP + SLG] %>% 
+    .[, TB := (H - B2 - B3 - HR) + (B2*2) + (B3 * 3) + (HR * 4)]
   
   return(DF)
 }
 
 
-# Scratch ::--------------------------------------
-
-DF <- fread(paste(fp, "Master.csv", sep = "")) %>% 
-  .[, fullName := paste(nameFirst, nameLast, sep = " ")] %>% 
-  .[, birthDate := as.Date(paste(birthYear, birthMonth, birthDay, sep = "-"))] %>% 
-  .[is.na(deathMonth) == F, deathMonth := ifelse(nchar(deathMonth) == 1, paste("0", deathMonth, sep = ""), deathMonth)] %>% 
-  .[is.na(deathDay) == F, deathDay := ifelse(nchar(deathDay) == 1, paste("0", deathDay, sep = ""), deathDay)] %>% 
-  .[is.na(deathYear) == F, deathDate := as.Date(paste(deathYear, deathMonth, deathYear, sep = "-"))]
-
-
-DF[fullName == "Hank Aaron", (today() - birthDate)/365]
-
-
-# Things to do ::-----------------------
-# 1. Master table: paste together birth year, month, and date to make a date string
-# 2. Add new variable for age
