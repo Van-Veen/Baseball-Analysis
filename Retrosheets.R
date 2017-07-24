3772107cb3256435f963d93965a4977d3313d032
J Stewart <30293862+Van-Veen@users.noreply.github.com>
2017-07-24 01:10:02 -0400

    Update Retrosheets.R

--- Retrosheets.R
+++ Retrosheets.R
@@ -3,15 +3,14 @@ library(data.table)
 library(magrittr)
 
 
-fp <- "C:/Users/jstewart/Downloads/2016eve/2016LAN.EVN"
+fpw <- "C:/Users/jstewart/Downloads/2016eve/2016LAN.EVN"
+fp <- "/Users/joelstewart/Desktop/Baseball Analysis/Data/2016eve/2016LAN.EVN"
 
 playCols <- c("Idx", "Category", "Inning", "HomeTeam", "RetroID", "Count", "Results", "Play" )
 
 DF <- readLines(fp) %>% 
   .[-grep("version,", .)] %>% 
-  paste(seq(1, length(.), by = 1), ., sep = ",") %>% 
-  .[grep("play,", .)] %>% 
-  .[-grep("com,", .)]
+  paste(seq(1, length(.), by = 1), ., sep = ",") 
 
 
 PLAYS <- readLines(fp) %>% 
@@ -26,6 +25,26 @@ PLAYS <- readLines(fp) %>%
   setnames(., c(paste("V", seq(1,ncol(.),1), sep = "")), playCols) %>% 
   .[, Idx := as.integer(Idx)]
 
+PR <- PLAYS[, c("Idx", "Results", "Play"), with = F] %>% 
+  .[, New := strsplit(Results, "")] %>%
+  .[, New := paste(Idx, New, sep = ",")] %>% 
+  .[, New := gsub("character\\(0\\)", "NP", New)] %>% 
+  .[, New := gsub("c\\(", "", New)] %>% 
+  .[, New := gsub("\\)", "", New)] %>% 
+  .[, New := gsub("\"", "", New)] %>% 
+  .[, New := gsub(" ", "", New)]
+
+write.table(PR$New, "/Users/joelstewart/Desktop/Test.csv", sep = ",", col.names = F, row.names = F, quote = F)
+
+PR2 <- read.csv("/Users/joelstewart/Desktop/Test.csv", 
+                 col.names = c("Idx", paste("P", seq(1, 16, 1), sep = "")),
+                 fill = T, header = F) %>% 
+  data.table(.) %>% 
+  melt(., id.vars = "Idx") %>% 
+  .[order(Idx)] %>% 
+  .[value != ""]
+
+test <- merge(PLAYS, PR2, by = "Idx", all.x = T, all.y = T)
 
 GMID <- readLines(fp) %>% 
   .[-grep("version,", .)] %>% 
@@ -43,7 +62,10 @@ INFO <- readLines(fp) %>%
   paste(seq(1, length(.), by = 1), ., sep = ",") %>% 
   .[grep("info,", .)]
 
-
+START <- readLines(fp) %>% 
+  .[-grep("version,", .)] %>% 
+  paste(seq(1, length(.), by = 1), ., sep = ",") %>% 
+  .[grep("start,", .)]
 
 test <- merge(PLAYS, GMID, by = c("Idx", "Category"), all.x = T, all.y = T)
 
