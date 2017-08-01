f04d36d3cdb643845ba1b72c725d34b81f1c376c
J Stewart <30293862+Van-Veen@users.noreply.github.com>
2017-08-01 10:26:04 -0400

    Adding SlashLine Function
    Built out a function to produce a player's slash line, with capacity to aggregate by age, season, team, or just produce a career average.

--- Single Stats Functions.R
+++ Single Stats Functions.R
@@ -36,3 +36,37 @@ batting_avg <- function(playerName, atWork = F, agg = NULL){
   return(avg)
 }
 
+slashLine <- function(playerName, atWork = F, agg = NULL){
+  
+  t1 <- batting(playerName, atWork = atWork) %>% 
+    .[, c("playerID", "yearID", "teamID", "lgID", "Age", "BA", "OBP", "SLG"), with = F] %>% 
+    .[, BAm := round(mean(BA), 3), by = agg] %>% 
+    .[, OBPm := round(mean(OBP), 3), by = agg] %>% 
+    .[, SLGm := round(mean(SLG), 3), by = agg]
+  
+  if(is.null(agg)){
+    
+    t1 <- t1[, -c("BA", "OBP", "SLG"), with = F] %>% 
+      .[yearID == max(yearID)] %>% 
+      .[, slashLine := paste(BAm, OBPm, SLGm, sep = "/")] %>% 
+      .[, c("playerID", "slashLine"), with = F]
+    
+  }else{
+    
+    t1 <- t1[, c("playerID", agg, "BAm", "OBPm", "SLGm"), with = F] %>% 
+      .[, slashLine := paste(BAm, OBPm, SLGm, sep = "/")] %>% 
+      .[, -c("OBPm", "BAm", "SLGm"), with = F]
+    
+    if(agg == "teamID"){
+      
+      t1 <- unique(t1)
+      
+    }
+    
+  }
+  
+  return(t1)
+  
+}
+
+
